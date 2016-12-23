9e96368d3572f0e2a2ce30f18e1e15754461e397
ivan0124 <ivan0124.tw@gmail.com>
2016-12-23 10:00:34 +0800

    update scriptx

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #MQTT_IMAGE=advigw4x86/mqtt-bus
 MQTT_IMAGE=ivan0124tw/docker_mqtt:v0.0.1
-MQTT_CONTAINER=mqtt
+MQTT_CONTAINER=advigw-mqtt-bus
 #DOCKER_API_GW_IMAGE=ivan0124tw/docker_api_gw:v0.0.2
-DOCKER_API_GW_IMAGE=ivan0124tw/docker_alpine_api_gw:v0.0.2
+DOCKER_API_GW_IMAGE=ivan0124tw/docker_alpine_api_gw
 DOCKER_API_GW_CONTAINER=docker_api_gw
 ADVANTECH_NET=advigw_network
 WSN_SETTING_FOLDER=advan_wsn_setting
@@ -75,9 +75,9 @@ fi
 echo "======================================="
 echo "[Step6]: Run container images......"
 echo "======================================="
-sudo docker run --network=$ADVANTECH_NET -itd --name $MQTT_CONTAINER -p 1883:1883 $MQTT_IMAGE
+sudo docker run -d -it --name $MQTT_CONTAINER -p 1883:1883 $MQTT_IMAGE
 #sudo docker run --network=$ADVANTECH_NET -it --name $DOCKER_API_GW_CONTAINER $DOCKER_API_GW_IMAGE
-sudo docker run --network=$ADVANTECH_NET -it --name $DOCKER_API_GW_CONTAINER -v $PWD/APIGateway:/home/adv/APIGateway:rw -v /usr/share/webmin/$WSN_SETTING_FOLDER:/home/adv/wsn_setting:rw -p 3000:3000 $DOCKER_API_GW_IMAGE
+sudo docker run -d -it --name $DOCKER_API_GW_CONTAINER -v $PWD/APIGateway:/home/adv/APIGateway:rw -v /usr/share/webmin/$WSN_SETTING_FOLDER:/home/adv/wsn_setting:rw -p 3000:3000 $DOCKER_API_GW_IMAGE
 
 
 if [ "$1" != "restart" ] ; then
@@ -90,3 +90,10 @@ echo "======================================="
 echo "[Step7]: Restart. Don't Clear Webmin module cache......"
 echo "======================================="
 fi
+
+#join to user-defined network advigw_network
+echo "======================================="
+echo "[Step8]: Join to network advigw_network......"
+echo "======================================="
+sudo docker network connect $ADVANTECH_NET $MQTT_CONTAINER
+sudo docker network connect $ADVANTECH_NET $DOCKER_API_GW_CONTAINER
