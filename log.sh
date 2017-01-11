08d25e3780e5e4f675f07273048818109b2c15e1
ivan0124 <ivan0124.tw@gmail.com>
2017-01-11 11:49:37 +0800

    update file

--- log.sh
+++ log.sh
@@ -7,5 +7,6 @@ CONTAINER_ID=`sudo docker ps | grep $CONTAINER_NAME | awk '{ print $1}'`
 
 #get container logs
 sudo docker logs $CONTAINER_ID > $LOG_NAME
+cat $LOG_NAME
 
 echo "output log name is  $LOG_NAME"
